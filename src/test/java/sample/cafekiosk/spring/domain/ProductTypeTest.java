8a2e20d73b169fb9c69aeef0ea2cfc9d32b86cdd
kimy12 <shinkim7941@gmail.com>
2024-12-02 20:26:06 +0900

    add : @ParameterizedTest

--- src/test/java/sample/cafekiosk/spring/domain/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/ProductTypeTest.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +21,18 @@ class ProductTypeTest {
 
         //then
         assertThat(result).isTrue();
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @CsvSource({"HANDMADE,false","BOTTLE,true","BAKERY,true"})
+    @ParameterizedTest
+    void containsStockType4(ProductType productType, boolean expected){
+
+        //when
+        boolean result = ProductType.containsStockType(productType);
+
+        //then
+        assertThat(result).isEqualTo(expected);
     }
 
 }
